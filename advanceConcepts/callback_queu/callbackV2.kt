1714af186be8c4c9cb4eff5a35d67e641af0be00
ashroaban003 <ratnakumar48070@gmail.com>
2025-11-29 21:29:02 +0530

    overriding_ability

--- advanceConcepts/callback_queu/callbackV2.kt
+++ advanceConcepts/callback_queu/callbackV2.kt
@@ -3,18 +3,39 @@ interface callcenter{
     fun onSuccess(result : String)
     fun onFailure(error : String)
 }
+class mana(val nums : Int){
+    fun printnum(){
+        println("birthday tmrw ${nums}" )
+    }
+    // fun end()
+}
 
-fun executeTask(listener: callcenter){
+open class keerthi(val nums :Int){
+    open fun igUser(){
+        println("she blocked ${nums}")
+    }
+}
+
+abstract class shreya(){
+    abstract fun finals()
+}
+
+fun executeTask(listener: callcenter,ender: mana,finisher: shreya,blocker:keerthi){
     println("task started")
     listener.onProcess(0)
     listener.onSuccess("hey")
     listener.onFailure("not failed")
+
+    //class
+    ender.printnum()
+    finisher.finals()
+    blocker.igUser()
 }
 
 fun main(){
     executeTask(object : callcenter {
         override fun onProcess(process: Int) {
-            println("▶️ Progress: $progress%")
+            println("▶️ Progress: $process%")
         }
 
         override fun onSuccess(result: String) {
@@ -24,5 +45,15 @@ fun main(){
         override fun onFailure(error: String) {
             println("❌ Failure! Error details: $error")
         }
-    })
+    },mana(30)
+    ,object : shreya(){
+        override fun finals(){
+            println("shreya")
+        }
+    },object: keerthi(6){
+        override fun igUser(){
+            println("she blocked ${nums*3}")
+        }
+    }
+    )
 }
