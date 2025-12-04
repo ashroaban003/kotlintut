4e70efc81ef16e420b87fa98b86c1012c88387a3
ashroaban003 <ratnakumar48070@gmail.com>
2025-12-04 22:06:41 +0530

    revisit

--- advanceConcepts/TypeAlias/TypeAlias.kt
+++ advanceConcepts/TypeAlias/TypeAlias.kt
@@ -1,31 +1,12 @@
-typealias Callback = (Int, String, Boolean) -> Unit
+typealias callback = (Int , Int , Int) -> Int;
 
-fun process(handler: Callback) {
-    println("Starting the process...")
-    val code = 200
-    val message = "Success"
-    val status = true
-
-    // Call the handler function with the required arguments
-    handler(code, message, status)
-
-    println("Process finished.")
+fun process(x : Int,mana : callback){
+    println(mana(x,x+1,x+2));
 }
 
-// Example of calling the 'process' function with a lambda
+
 fun main() {
-    process { number, text, isActive ->
-        println("--- Handler Executed ---")
-        println("Received Number: $number")
-        println("Received Text: $text")
-        println("Received Status: $isActive")
-        // The lambda implicitly returns Unit
+    process(9){ a,b,c ->
+        a+b+c
     }
 }
-// Output:
-// Starting the process...
-// --- Handler Executed ---
-// Received Number: 200
-// Received Text: Success
-// Received Status: true
-// Process finished.
