c22e5c023691f385f244c1d2d22214d76a53c666
ashroaban003 <ratnakumar48070@gmail.com>
2025-12-08 19:29:01 +0530

    conditional_witeOnly

--- advanceConcepts/Delegation/Adv_v1.kt
+++ advanceConcepts/Delegation/Adv_v1.kt
@@ -10,9 +10,15 @@ var name: String by Delegates.observable("Initial Name") {
     prop, old, new -> 
     println("Property ${prop.name} changed from '$old' to '$new'")
 }
+var age:Int by Delegates.vetoable(18){
+    prop,old,new->
+    new>=20 //effectife from now
+}
 fun main(){
     println(expensiveComputaion)
     println(expensiveComputaion)
     name = "mana"
     name = "shreya"
+    age = -3
+    println(age)
 }
