30862e1ad85835e09e11c68e32df8be5790782dd
ashroaban003 <ratnakumar48070@gmail.com>
2025-12-15 17:08:46 +0530

    context_receiver1

--- advanceConcepts/ContextReceiver/crSample1.kt
+++ advanceConcepts/ContextReceiver/crSample1.kt
@@ -1,12 +1,30 @@
 class Logger{
     fun log() = println("nah! I win")
 }
+class Database {
+    fun save(data: String) = println("Saved: $data")
+}
+//our service
 class UserService1(private val logger : Logger){
     fun createUser(){
         logger.log()
     }
 }
-
+context(Logger, Database)
+fun createUser(name: String) {
+    log()   // Logger available
+    save(name)                   // Database available
+}
 fun main(){
     val logger = Logger()
+    val database = Database()
+    val userService1 = UserService1(logger)
+    userService1.createUser()
+   //context receiver
+   with(logger){
+    with(database){
+        createUser("Shreya")
+    }
+   }
+
 }
